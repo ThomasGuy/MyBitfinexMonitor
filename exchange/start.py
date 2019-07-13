aeb998ef63933df4ab400212d81dcfbbaf0cad76
ThomasGuy <TWGuy66@gmail.com>
2019-07-13 07:17:10 +0700

    use new bfxapi

--- exchange/start.py
+++ exchange/start.py
@@ -2,24 +2,41 @@ import asyncio
 import os
 from wxasync import WxAsyncApp
 # package imports
-from .BFX import wsbfx, Client
+from .BFX import wsbfx, Client, CustomLogger
 from .wx_gui import WalletFrame, WalletEvent
 
 
+log = CustomLogger('start', 'DEBUG')
+
 api_key = os.getenv('BFX_KEY')
 api_secret = os.getenv('BFX_SECRET')
+loglevel = 'DEBUG'
+
+
+class AuthError(Exception):
+    """
+    Thrown whenever there is a problem with the authentication packet
+    """
+    pass
 
 
 def main_async():
-    loop = asyncio.events.get_event_loop()
-    bfx = Client(API_KEY=api_key, API_SECRET=api_secret, loop=loop)
-    app = WxAsyncApp(loop=loop)
-    frame = WalletFrame()
-    frame.Show()
-    app.SetTopWindow(frame)
-    tasks = [
-        asyncio.ensure_future(bfx.ws.get_task_executable()),
-        asyncio.ensure_future(app.MainLoop()),
-    ]
-    wsbfx(bfx, frame, WalletEvent)
-    loop.run_until_complete(asyncio.wait(tasks))
+    try:
+        loop = asyncio.events.get_event_loop()
+        bfx = Client(API_KEY=api_key, API_SECRET=api_secret, loop=loop)
+        app = WxAsyncApp(loop=loop)
+        frame = WalletFrame()
+        frame.Show()
+        app.SetTopWindow(frame)
+        tasks = [
+            asyncio.ensure_future(bfx.ws.get_task_executable()),
+            asyncio.ensure_future(app.MainLoop()),
+        ]
+        wsbfx(bfx, frame, WalletEvent)
+        loop.run_until_complete(asyncio.wait(tasks))
+    except AuthError as err:
+        log.error(err)
+    except Exception:
+        log.error('Exception ', exc_info=True)
+    else:
+        log.onfo('Bitfinex user is logging in...')
